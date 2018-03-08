fad35541de3840179f8e1e2d791e6a3ec80771fe
dsh <xfhngfn>
2018-03-09 00:12:21 +0800

    sac

--- SpringBoot-jsp_02/src/main/java/cn/itcast/controller/ItemController.java
+++ SpringBoot-jsp_02/src/main/java/cn/itcast/controller/ItemController.java
@@ -9,6 +9,7 @@ public class ItemController {
 
 	@RequestMapping("/item")
 	public String item(Model model) {
+		System.out.println("11111111111111");
 		model.addAttribute("itemArr", new String[]{
 				"小米","华为0","苹果","Opp"
 		});
